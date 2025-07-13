aa1480ae07d2d53388ab0f57a6462f9b187cf00e
prabashwara65 <prabashwara65@gmail.com>
2025-07-13 10:33:42 +0530

    commit

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.get("/" , ( req , res ) =>{
     res.json("backend working with api ")
 })
 
-app.listen(4000 , () => {
-    console.log("server is running on port 4000")
+app.listen(process.env.PORT , () => {
+    console.log("server is running on port " + process.env.PORT)
 })
